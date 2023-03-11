b5eafc828ec3fc2dd408a8f141e820a7cc24b0fd
Kai Yamada <m5251154@u-aizu.ac.jp>
2023-03-11 15:32:26 +0900

    setup x11

--- docker-run.sh
+++ docker-run.sh
@@ -4,6 +4,7 @@ DISTRO="nano-5.11"
 . ./${DISTRO}/env.sh
 
 docker run -it --rm \
+    --net=host --env="DISPLAY" --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume ${HOME}:${HOME} \
     --volume $(pwd)/out:${DOCKER_WORKDIR} \
     --volume $(pwd)/${DISTRO}:${DOCKER_WORKDIR}/${DISTRO} \
