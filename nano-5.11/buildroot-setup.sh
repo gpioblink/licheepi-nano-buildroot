e5b9cd201e7cf736914358553f0ed6af9000b7c6
Kai Yamada <m5251154@u-aizu.ac.jp>
2023-03-11 16:59:20 +0900

    get br2

--- nano-5.11/buildroot-setup.sh
+++ nano-5.11/buildroot-setup.sh
@@ -8,7 +8,12 @@ if [ ! -d ${DOCKER_WORKDIR}/licheepi-nano-sdk ]; then
 	cp -r ${BUILDROOT_OVERRIDES_DIR}/* ${DOCKER_WORKDIR}/buildroot
 fi
 
+if [ ! -d ${DOCKER_WORKDIR}/licheepi-nano ]; then
+	cp -r ${BUILDROOT_OVERRIDES_DIR}/* ${DOCKER_WORKDIR}/licheepi-nano
+fi
+
 cd ${DOCKER_WORKDIR}/buildroot
+BR2_EXTERNAL=${DOCKER_WORKDIR}/licheepi-nano make licheepi_nano_defconfig
 make toolchain
 
 make linux-source
